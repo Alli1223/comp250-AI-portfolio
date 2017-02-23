14edb38580e8f8196f6ae9af3fd79588459783f8
Alastair Rayner <alli0@me.com>
2017-02-23 03:53:43 +0000

    Added Agent Behaviour class

---
+++ SpaceGame/SDL_project/AgentBehaviour.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "AgentBehaviour.h"
+
+
+AgentBehaviour::AgentBehaviour()
+{
+}
+
+
+AgentBehaviour::~AgentBehaviour()
+{
+}
---
+++ SpaceGame/SDL_project/AgentBehaviour.h
@@ -0,0 +1,8 @@
+#pragma once
+class AgentBehaviour
+{
+public:
+	AgentBehaviour();
+	~AgentBehaviour();
+};
+
