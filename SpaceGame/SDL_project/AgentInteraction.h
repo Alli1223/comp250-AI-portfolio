8403a9d5a0f40d890004337865d75e0e36c66d57
Alastair Rayner <alli0@me.com>
2017-03-26 19:46:14 +0100

    Made some variables constant and fixed some errors with agent manager

--- SpaceGame/SDL_project/AgentInteraction.h
+++ SpaceGame/SDL_project/AgentInteraction.h
@@ -8,18 +8,5 @@ public:
 	PlayerInteraction();
 	~PlayerInteraction();
 
-	void PlayerInteraction::Interaction(Level& grid, Agent& agent);
-
-	//!Get door status
-	bool getDoorStatus() { return doorStatus; }
-
-	//!Set door status
-	bool setDoorStatus(bool newDoorStatus) { return doorStatus = newDoorStatus; }
-
-private:
-	//!Door status - True is open - False is closed
-	bool doorStatus = false;
-	int FireExtinguisherEffectiveness = 10;
-	int doorOpenDistance = 3;
 };
 
